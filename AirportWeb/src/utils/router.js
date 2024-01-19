733f951d28b23f62102483665e6f04e4fe8dcaba
Szidor Patrik <patrikp987@gmail.com>
2024-01-19 12:40:04 +0100

    created summary route and component

--- AirportWeb/src/utils/router.js
+++ AirportWeb/src/utils/router.js
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Booking from '@components/Booking.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Booking from '@components/Booking.vue'
+import Summary from '@components/Summary.vue'
 
 const routes = [
   { path: '/booking', component: Booking },
+  { path: '/summary', component: Summary },
 ];
 
 const router = createRouter({
