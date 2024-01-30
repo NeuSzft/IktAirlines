feb61fe43b8cb2225fd26dcda8ca2c36c12efaaa
Szidor Patrik <patrikp987@gmail.com>
2024-01-30 11:36:28 +0100

    added @pages alias

--- AirportWeb/vite.config.js
+++ AirportWeb/vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
         alias: {
             "@": fileURLToPath(new URL('./src', import.meta.url)),
             "@utils": fileURLToPath(new URL('./src/utils', import.meta.url)),
-            "@components": fileURLToPath(new URL('./src/components', import.meta.url))
+            "@components": fileURLToPath(new URL('./src/components', import.meta.url)),
+            "@pages": fileURLToPath(new URL('./src/pages', import.meta.url))
         }
     }
 })
