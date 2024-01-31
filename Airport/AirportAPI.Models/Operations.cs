83f0a3d78f8f628ecb87bf86beea9123035262fe
Törőcsik Ádám <toarexer@gmail.com>
2024-01-31 19:59:36 +0100

    Added /modify endpoints

---
+++ Airport/AirportAPI.Models/Operations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AirportAPI.Models;
+
+public abstract class OperationInfo(string operationName, string modelName) {
+    [JsonPropertyName("operation_name"), JsonRequired]
+    public string OperationName { get; set; } = operationName;
+
+    [JsonPropertyName("model_name"), JsonRequired]
+    public string ModelName { get; set; } = modelName;
+
+    [JsonPropertyName("id"), JsonRequired]
+    public int Id { get; set; }
+
+    [JsonPropertyName("item"), JsonRequired]
+    public object Item { get; set; } = null!;
+}
+
+public abstract record Operation {
+    public static Operation? FromJson(JsonElement operationInfoElement) {
+        JsonElement operationName = operationInfoElement.GetProperty("operation_name");
+        JsonElement modelName = operationInfoElement.GetProperty("model_name");
+        JsonElement id = operationInfoElement.GetProperty("id");
+        JsonElement item = operationInfoElement.GetProperty("item");
+
+        Assembly assembly = typeof(Operation).Assembly;
+        string? assemblyName = assembly.GetName().Name;
+
+        Type? operationType = assembly.GetType($"{assemblyName}.{operationName.GetString()}");
+        Type? modelType = assembly.GetType($"{assemblyName}.{modelName.GetString()}");
+
+        if (operationType is null || modelType is null)
+            return null;
+
+        Type type = operationType.MakeGenericType(modelType);
+
+        Operation? operation = Activator.CreateInstance(type) as Operation;
+        type.GetProperty("Id")?.SetValue(operation, id.GetInt32());
+        type.GetProperty("Item")?.SetValue(operation, item.Deserialize(modelType));
+
+        return operation;
+    }
+
+    public static IEnumerable<Operation> FromJson(JsonDocument document) {
+        if (document.RootElement.ValueKind == JsonValueKind.Array)
+            return document.RootElement.EnumerateArray().Select(FromJson).Where(x => x is not null)!;
+        return [];
+    }
+}
+
+public sealed record AddOperation<T> : Operation where T : IdModel {
+    public T Item { get; internal set; } = null!;
+}
+
+public sealed record UpdateOperation<T> : Operation where T : IdModel {
+    public int Id { get; internal set; }
+    public T Item { get; internal set; } = null!;
+}
+
+public sealed record RemoveOperation<T> : Operation where T : IdModel {
+    public int Id { get; internal set; }
+}
+
+public class UnknownOperationException(object? operation) : Exception {
+    public override string Message => $"{operation?.GetType()} is an unknown operation";
+}
--- Airport/AirportAPI/Endpoints/Other.cs
+++ Airport/AirportAPI/Endpoints/Other.cs
@@ -1,17 +1,78 @@
-﻿using Dapper;
+﻿using AirportAPI.Models;
+using AirportAPI.Models.Testing;
+using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using System.Linq;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace AirportAPI.Endpoints;
 
 public static class Other {
-    public static int? GetNextId(this DatabaseConnection db, string table) {
-        string query = $"SELECT last_value FROM {table}_id_seq;";
+    public static (Exception?, int) PerformOperations(this NpgsqlConnection connection, IEnumerable<Operation> operations) {
+        int n = 0;
 
-        using var con = db.Open();
-        return con.Query<int>(query).FirstOrDefault();
+        try {
+            foreach (Operation operation in operations) {
+                {
+                    if (operation is AddOperation<Airline> add) {
+                        n += connection.PostAirline(add.Item);
+                        continue;
+                    }
+                    if (operation is UpdateOperation<Airline> update) {
+                        n += connection.PutAirline(update.Id, update.Item);
+                        continue;
+                    }
+                    if (operation is RemoveOperation<Airline> remove) {
+                        n += connection.DelAirline(remove.Id);
+                        continue;
+                    }
+                }
+                {
+                    if (operation is AddOperation<City> add) {
+                        n += connection.PostCity(add.Item);
+                        continue;
+                    }
+                    if (operation is UpdateOperation<City> update) {
+                        n += connection.PutCity(update.Id, update.Item);
+                        continue;
+                    }
+                    if (operation is RemoveOperation<City> remove) {
+                        n += connection.DelCity(remove.Id);
+                        continue;
+                    }
+                }
+                {
+                    if (operation is AddOperation<Flight> add) {
+                        n += connection.PostFlight(add.Item);
+                        continue;
+                    }
+                    if (operation is UpdateOperation<Flight> update) {
+                        n += connection.PutFlight(update.Id, update.Item);
+                        continue;
+                    }
+                    if (operation is RemoveOperation<Flight> remove) {
+                        n += connection.DelFlight(remove.Id);
+                        continue;
+                    }
+                }
+
+                throw new UnknownOperationException(operation);
+            }
+        } catch (UnknownOperationException e) {
+            return (e, 0);
+        } catch (NpgsqlException e) {
+            return (e, 0);
+        }
+
+        return (null, n);
+    }
+
+    public static int? GetNextId(this NpgsqlConnection connection, string table) {
+        return connection.QueryFirstOrDefault<int?>($"SELECT last_value FROM {table}_id_seq;");
     }
 
     public static void MapOther(this WebApplication app) {
@@ -22,7 +83,8 @@ public static class Other {
         .Produces(StatusCodes.Status200OK);
 
         app.MapGet("/next-id/airlines", (DatabaseConnection db) => {
-            int? id = db.GetNextId("airlines");
+            using NpgsqlConnection connection = db.Open();
+            int? id = connection.GetNextId("airlines");
             return id is null ? Results.NotFound() : Results.Ok(id + 1);
         })
         .WithName("Get next available Airline Id")
@@ -32,7 +94,8 @@ public static class Other {
         .Produces(StatusCodes.Status404NotFound);
 
         app.MapGet("/next-id/cities", (DatabaseConnection db) => {
-            int? id = db.GetNextId("cities");
+            using NpgsqlConnection connection = db.Open();
+            int? id = connection.GetNextId("cities");
             return id is null ? Results.NotFound() : Results.Ok(id + 1);
         })
         .WithName("Get next available City Id")
@@ -42,7 +105,8 @@ public static class Other {
         .Produces(StatusCodes.Status404NotFound);
 
         app.MapGet("/next-id/flights", (DatabaseConnection db) => {
-            int? id = db.GetNextId("flights");
+            using NpgsqlConnection connection = db.Open();
+            int? id = connection.GetNextId("flights");
             return id is null ? Results.NotFound() : Results.Ok(id + 1);
         })
         .WithName("Get next available Flight Id")
@@ -50,5 +114,51 @@ public static class Other {
         .WithOpenApi()
         .Produces<int>(StatusCodes.Status200OK, "text/plain")
         .Produces(StatusCodes.Status404NotFound);
+
+        app.MapPatch("/modify", async (DatabaseConnection db, HttpRequest request) => {
+            using JsonDocument document = await JsonDocument.ParseAsync(request.Body);
+            IEnumerable<Operation> operations = Operation.FromJson(document);
+
+            using NpgsqlConnection connection = db.Open();
+            using NpgsqlTransaction transaction = connection.BeginTransaction();
+
+            (Exception? exception, int affected) = PerformOperations(connection, operations);
+
+            if (exception is null) {
+                transaction.Commit();
+                return Results.Ok(affected);
+            }
+
+            transaction.Rollback();
+            return Results.UnprocessableEntity(exception.Message);
+        })
+        .WithName("Perform a transaction then roll back on error")
+        .WithTags("Other Endpoints")
+        .WithOpenApi()
+        .Accepts<IEnumerable<OperationInfo>>("application/json")
+        .Produces<int>(StatusCodes.Status200OK, "text/plain")
+        .Produces(StatusCodes.Status400BadRequest)
+        .Produces<string>(StatusCodes.Status422UnprocessableEntity, "text/plain");
+
+        app.MapPatch("/modify/test", async (DatabaseConnection db, HttpRequest request) => {
+            using JsonDocument document = await JsonDocument.ParseAsync(request.Body);
+            IEnumerable<Operation> operations = Operation.FromJson(document);
+
+            using NpgsqlConnection connection = db.Open();
+            using NpgsqlTransaction transaction = connection.BeginTransaction();
+
+            (Exception? exception, int affected) = PerformOperations(connection, operations);
+            ModificationResults result = new(affected, connection.GetAirlines(), connection.GetCities(), connection.GetFlights());
+
+            transaction.Rollback();
+            return exception is null ? Results.Ok(result) : Results.UnprocessableEntity(exception.Message);
+        })
+        .WithName("Perform a transaction then roll back")
+        .WithTags("Other Endpoints")
+        .WithOpenApi()
+        .Accepts<IEnumerable<OperationInfo>>("application/json")
+        .Produces<ModificationResults>(StatusCodes.Status200OK, "application/json")
+        .Produces(StatusCodes.Status400BadRequest)
+        .Produces<string>(StatusCodes.Status422UnprocessableEntity, "text/plain");
     }
 }
