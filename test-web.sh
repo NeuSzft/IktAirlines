2d81f711bed80f2f9af0a386c4b34a6424711479
Szidor Patrik <patrikp987@gmail.com>
2024-02-01 10:47:19 +0100

    removed ports

--- test-web.sh
+++ test-web.sh
@@ -3,4 +3,4 @@ docker compose -f compose.test-web.yml down
 docker compose -f compose.test-web.yml build api-build-test selenium-tests
 docker compose -f compose.test-web.yml up -d
 docker compose -f compose.test-web.yml logs -f selenium-tests
-#docker compose -f compose.test-web.yml down
+docker compose -f compose.test-web.yml down
