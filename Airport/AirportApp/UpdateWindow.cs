9acede771e5a8cbe9a9c2ca68c55cdb80929bd72
Szidor Patrik <patrikp987@gmail.com>
2024-02-06 14:36:55 +0100

    initialized _updatinfo and set some properties

--- Airport/AirportApp/UpdateWindow.cs
+++ Airport/AirportApp/UpdateWindow.cs
@@ -9,13 +9,23 @@ namespace AirportApp
 
     public class UpdateWindow : Window
     {
+        private readonly UpdateInfo _updateInfo;
         public UpdateWindow(UpdateInfo updateInfo)
         {
+            _updateInfo = updateInfo;
             InitializeComponents();
         }
 
         private void InitializeComponents()
         {
+            Title = "Update Information";
+            MinWidth = 300;
+            MaxWidth = 900;
+            MinHeight = 200;
+            MaxHeight = 600;
+            SizeToContent = SizeToContent.WidthAndHeight;
+            ResizeMode = ResizeMode.CanMinimize;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
     }
 }
