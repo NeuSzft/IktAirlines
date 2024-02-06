02fa269626ca3d7cf6585c45fd56773649c1b2c9
Szidor Patrik <patrikp987@gmail.com>
2024-02-06 14:33:43 +0100

    created UpdateWindow.cs

---
+++ Airport/AirportApp/UpdateWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AirportApp
+{
+    public record UpdateInfo(List<string> AddedItems, List<string> UpdatedItems, List<string> RemovedItems);
+
+    public class UpdateWindow : Window
+    {
+        public UpdateWindow(UpdateInfo updateInfo)
+        {
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+        }
+    }
+}
