288fdee3ac6a125a54052b954c411954f4aa83a9
Törőcsik Ádám <toarexer@gmail.com>
2024-02-06 23:43:56 +0100

    R E S T

---
+++ AirportManagement/AirportApp/ManagementWindow.cs
@@ -0,0 +1,106 @@
+﻿using AirportAPI.Models;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shell;
+
+namespace AirportApp;
+
+internal sealed class ManagementWindow : Window {
+    public ManagementWindow() {
+        RequestHelper helper = new("http://localhost:5000");
+
+        TableTab<Airline> airlinesTab = new(
+            "Airlines",
+            new() {
+                Get = () => helper.Get<Airline>("/airlines"),
+                Post = (airline) => helper.Post("/airlines", airline),
+                Put = (id, airline) => helper.Put($"/airlines/{id}", airline),
+                Delete = (id) => helper.Delete($"/airlines/{id}"),
+                NextId = () => helper.NextId("/next-id/airlines"),
+                Modify = helper.Modify
+            },
+            new DataGridTextColumn { Header = "Id", IsReadOnly = true, Binding = new Binding("Id") },
+            new DataGridTextColumn { Header = "Name", Binding = new Binding("Name") }
+        );
+
+        TableTab<City> citiesTab = new(
+            "Cities",
+            new() {
+                Get = () => helper.Get<City>("/cities"),
+                Post = (city) => helper.Post("/cities", city),
+                Put = (id, city) => helper.Put($"/cities/{id}", city),
+                Delete = (id) => helper.Delete($"/cities/{id}"),
+                NextId = () => helper.NextId("/next-id/cities"),
+                Modify = helper.Modify
+            },
+            new DataGridTextColumn { Header = "Id", IsReadOnly = true, Binding = new Binding("Id") },
+            new DataGridTextColumn { Header = "Name", Binding = new Binding("Name") },
+            new DataGridTextColumn { Header = "Population", Binding = new Binding("Population") }
+        );
+
+        CustomGridComboBoxColumn<Airline> airlineIdColumn = new(airlinesTab.Grid.ItemList) { Header = "Airline Id", SelectedValueBinding = new Binding("AirlineId") };
+        CustomGridComboBoxColumn<City> originIdColumn = new(citiesTab.Grid.ItemList) { Header = "Origin City Id", SelectedValueBinding = new Binding("OriginId") };
+        CustomGridComboBoxColumn<City> destinationIdColumn = new(citiesTab.Grid.ItemList) { Header = "Destination City Id", SelectedValueBinding = new Binding("DestinationId") };
+
+        TableTab<Flight> flightsTab = new(
+            "Flights",
+            new() {
+                Get = () => helper.Get<Flight>("/flights"),
+                Post = (flight) => helper.Post("/flights", flight),
+                Put = (id, flight) => helper.Put($"/flights/{id}", flight),
+                Delete = (id) => helper.Delete($"/flights/{id}"),
+                NextId = () => helper.NextId("/next-id/flights"),
+                Modify = helper.Modify
+            },
+            new DataGridTextColumn { Header = "Id", IsReadOnly = true, Binding = new Binding("Id") },
+            airlineIdColumn,
+            originIdColumn,
+            destinationIdColumn,
+            new DataGridTextColumn { Header = "Distance", Binding = new Binding("Distance") },
+            new DataGridTextColumn { Header = "Flight Time (minutes)", Binding = new Binding("FlightTime") },
+            new DataGridTextColumn { Header = "Ft/Km", Binding = new Binding("HufPerKm") }
+        );
+
+        airlinesTab.Grid.ItemList.CollectionChanged += (_, _) => airlineIdColumn.ItemsSource = airlinesTab.Grid.ItemIds;
+        citiesTab.Grid.ItemList.CollectionChanged += (_, _) => {
+            originIdColumn.ItemsSource = citiesTab.Grid.ItemIds;
+            destinationIdColumn.ItemsSource = citiesTab.Grid.ItemIds;
+        };
+
+        HomeTab homeTab = new(helper, airlinesTab.FillDataGrids, citiesTab.FillDataGrids, flightsTab.FillDataGrids);
+
+        TabControl content = new();
+        content.SelectionChanged += (_, _) => {
+            foreach (Border border in content.Items.OfType<TabItem>().Select(x => x.Header).OfType<Border>()) {
+                bool selected = border.Equals((content.SelectedItem as TabItem)?.Header);
+                border.Background = selected ? Brushes.LightSkyBlue : Brushes.White;
+                border.BorderBrush = selected ? Brushes.LightSkyBlue : Brushes.LightGray;
+            }
+        };
+
+        content.Items.Add(homeTab);
+        content.Items.Add(airlinesTab);
+        content.Items.Add(citiesTab);
+        content.Items.Add(flightsTab);
+
+        Background = Brushes.Transparent;
+        MinWidth = 500;
+        MinHeight = 300;
+        Content = content;
+        Title = "Airlines Database Manager";
+
+        Closed += (_, _) => helper.Dispose();
+
+        foreach (IInputElement element in content.Items)
+            WindowChrome.SetIsHitTestVisibleInChrome(element, true);
+
+        WindowChrome.SetWindowChrome(this, new() {
+            ResizeBorderThickness = new(4),
+            GlassFrameThickness = new(0, 32, 0, 0),
+            CaptionHeight = 32
+        });
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Airlines Project
 
+### Table of Contents
+- [Overview](#overview)
+- [Database API](#database-api)
+- [Web Interface](#web-interface)
+- [Desktop Application](#desktop-application)
+- [Getting started](#getting-started)
+- [Testing](#testing)
+
+## Overview
+
 This project contains an API for managing a PostgreSQL database, a web interface for seeing available flights and their prices, and a desktop application that provides a graphical interface for managing the database trough API.
 
-## API
+## Database API
 
 The API is made using [APS.NET Core](https://github.com/dotnet/aspnetcore) and it's [Minimal APIs](https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis/overview?view=aspnetcore-8.0) approach.
 
@@ -15,3 +25,22 @@ The API is made using [APS.NET Core](https://github.com/dotnet/aspnetcore) and i
 The desktop's purpose is to make adding, updating and removing entries from the database a simple task.
 
 It is made using the [Windows Presentation Foundation](https://github.com/dotnet/wpf) framework and is only available for windows machines.
+
+## Getting started
+
+The database, API and web server can be run using [docker compose](https://github.com/docker/compose) and the [compose.yml](./compose.yml) compose file.
+Run `docker compose up` command to launch the containers.
+> It can also be done by running the [run.sh](./run.sh) shell script.
+
+Local ports used by the services:
+- 5432 - PostgreSQL database
+- 5000 - Database API
+- 8080 - Web server
+
+## Testing
+
+The tests can be run using [docker compose](https://github.com/docker/compose) and the appropriate compose file.
+
+To run the API tests run the `docker compose -f compose.test-api.yml up` command the [compose.test-api.yml](./compose.test-api.yml) file or run [test-api.sh](./test-api.sh) script.
+
+To run the web interface's [Selenium](https://www.selenium.dev/) tests use the [compose.test-web.yml](./compose.test-web.yml) file or run [test-web.sh](./test-web.sh) script.
